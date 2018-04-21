85acb0b9bb0da3450b466ee7a5d6849d8c7291a1
Harry Liu <byliuyang11@gmail.com>
2018-04-21 19:57:32 -0400

    Print out post body

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-  res.end(req.body);
+router.post('/', function (req, res, next) {
+    console.log(req.body);
+    res.end();
 });
 
 module.exports = router;
