cd029a55d530d5032900b7c85aa458a9e9239098
Harry Liu <byliuyang11@gmail.com>
2018-04-21 20:49:35 -0400

    Add auto pull on push

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const fs = require('fs');
+const exec = require('child_process').exec;
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
@@ -10,9 +11,21 @@ router.post('/', function (req, res, next) {
     switch (githubEvent) {
         case 'push':
             let repository = req.body['repository'];
-            fs.readFile('deployment.config.json', 'utf8', (err, data) => {
-                console.log(repository['full_name']);
-                console.log(data);
+            fs.readFile('deployment.config.json', 'utf8', (err, json) => {
+                let repoFullName = repository['full_name'];
+                let deploymentConfig = JSON.parse(json);
+
+                deploymentConfig.filter(config => config['repo_full_name'] === repoFullName)
+                    .forEach(config => {
+                       let repoLocalDir = config['repo_local_dir'];
+                       exec('git pull', {
+                           cwd: repoLocalDir
+                       }, (error, stdout, stderr) => {
+                           console.log('error', error);
+                           console.log('stdout', stdout);
+                           console.log('stderr', stderr);
+                       });
+                    });
                 res.end();
             });
             break;
